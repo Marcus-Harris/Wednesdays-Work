28879495dba6bc3a6d7cde6f1a290e561bb7b0af
Marcus-Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-02-24 15:50:15 -0500

    Wednesday's work

---
+++ If Statements.js
@@ -0,0 +1,17 @@
+// CHallenge 1: Do You Need More Coffee?
+let cupsOfCoffee = 2;
+
+if(cupsOfCoffee < 3) {
+    console.log("Yes I'll take another cup of coffee.")
+} else {
+    console.log("I think I'm okay for now.")
+}
+
+// Challenge 2: Does your car need an oil change?
+let carMileage = 800;
+
+if(carMileage > 500){
+    console.log("Yes I think I need an oil change.")
+} else {
+    console.log("My car doesn't need an oil change yet.")
+}
